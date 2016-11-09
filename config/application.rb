0df65e415bb4e4a92c479687bfd0939cec4c45ab
Thiago <thiagorova@gmail.com>
2016-11-09 17:26:55 -0200

    setting assets to precompile

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module MetricsDownload
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile +=  ['*.js', '*.css', '*.css.erb']
+    config.assets.precompile +=  ['application-main.css', 'application-downloads.css']
   end
 end
